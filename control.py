23ff44e9825195354c1997f4ad9ee7ae211380ce
JecintaNjoki <jecintanjokikimanii@gmail.com>
2023-04-20 22:54:19 +0300

    first commit

---
+++ control.py
@@ -0,0 +1,99 @@
+def  even_numbers():
+    x= range(10)
+    for i in x:
+        if i % 2==0:
+            print(i)
+
+def odd_numbers():
+    x= range(20)
+    for i in x:
+        if i % 2!=0:
+            print(i) 
+def divisible_by_five():
+    x= range(50)
+    for i in x:
+        if i % 5==0:
+            print(f"{i}is divisible by 5")
+        else :
+            print(f"{i} is not divisible by 5") 
+def multiple_comparison():
+    x = range(50)
+    for i in x:
+        if i % 5==0:
+            print (f"{i}is divisible by 5")
+        elif i % 7==0:
+            print (f"{i}is divisible by 7")
+        elif i % 7==0:
+            print (f"{i}is divisible by 9")
+        else: 
+            print (f"{i}is divisible by 5,7,or 9")
+def odd_or_even():
+    x =range (20)
+    for i in x:
+        if i % 2==0 and i %3==0:
+            print (f"{i}is divisible by both 2 and 3")
+        elif i % 2==0 or i %3==0:
+            print (f"{i}is divisible by either 2 or 3")
+        else: 
+            print (f"{i}is nor divisible by either 3 or 3 ")
+
+def while_loop():
+    x=1
+    while x<10:
+        print("Hello")
+        x+=1
+
+def break_statement():
+    x=1
+    while x<10:
+        print("Hello")
+        x+=1
+        if x==5:
+            break
+def continue_statement():
+    x=0
+    while x<=20:
+        x+=1
+        if x % 3==0:
+            continue 
+        print(x)
+#Write a function that uses while, if and continue statements to print 
+# all the even numbers between 0 and 50. 
+def all_even_nimbers():
+    x=0
+    while x<=50:
+        x+=1
+        if x % 2==0:
+            continue
+        print(x)
+
+    
+#Write a function that takes an integer argument and prints "Prime" 
+# if the number is prime, and "Not prime" if the number is not prime.
+def prime_numbers(i=7):
+    if(i%1==0 and i %7==0):
+         print(f"{i}is a prime number")
+    else:
+         print(f"{i}is not prime number")
+
+#Write a function that takes a list of integers as input and prints 
+# the sum of all the even numbers in the list.
+
+
+def sum_numbers(*num):
+    sum=0
+    for i in num:
+        if i %2==0:   
+             sum+=i
+    print (sum)  
+#Write a function that takes any two integers as input, 
+# and prints the sum of all the numbers between the two integers
+# (inclusive) that are divisible by 3.
+def divisible_by_3(num):
+    sum=0
+    for i in range(1,num+1):
+        if i %3 ==0:
+           num+=1
+    print(sum)
+
+
